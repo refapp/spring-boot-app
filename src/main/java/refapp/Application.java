135322e8e6a8cd13e42e7e2d90a5340efec7f23b
Gurpreet Singh <refapp.io@gmail.com>
2015-07-05 20:30:56 -0700

    Javadoc comments

--- src/main/java/refapp/Application.java
+++ src/main/java/refapp/Application.java
@@ -3,9 +3,16 @@ package refapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application starter class
+ */
 @SpringBootApplication
 public class Application 
 {
+	/**
+	 * Main entry method
+	 * @param args arguments
+	 */
     public static void main(String[] args)
     {
     	SpringApplication.run(Application.class, args);
--- src/main/java/refapp/controller/HomeController.java
+++ src/main/java/refapp/controller/HomeController.java
@@ -5,9 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Home controller
+ */
 @RestController
 public class HomeController
 {
+	/**
+	 * Home method
+	 * @return String Json
+	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String getHome()
 	{
