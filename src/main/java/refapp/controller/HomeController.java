f5726134025b8d763616052c14fed56140179d9c
Gurpreet Singh <refapp.io@gmail.com>
2015-07-06 21:15:46 -0700

    path variables

--- src/main/java/refapp/controller/HomeController.java
+++ src/main/java/refapp/controller/HomeController.java
@@ -1,6 +1,7 @@
 package refapp.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController
 {
+	private static final String CONTEXT_TYPE = "context_type";
+	private static final String CONTEXT_NAME = "context_name";
+	
 	/**
 	 * Home method
 	 * @return String Json
@@ -19,5 +23,11 @@ public class HomeController
 	public String getHome()
 	{
 		return "Hello World";
+	}
+	
+	@RequestMapping(value="{" + CONTEXT_TYPE + "}/{" + CONTEXT_NAME + ":.+}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String processContext(@PathVariable(CONTEXT_TYPE) String contextType, @PathVariable(CONTEXT_NAME) String contextName)
+	{
+		return "CONTEXT TYPE : " + contextType + ", CONTEXT NAME : " + contextName;
 	}
 }
